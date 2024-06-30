69fe31b2a23331247a1688e422c83f302ac2b24b
Javier Martin Carballares <jcarbal@seas.upenn.edu>
2024-06-30 02:08:40 +0000

    new url

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -27,7 +27,7 @@ def connect_with_middleware(contract_json):
 	# TODO complete this method
 	# The first section will be the same as "connect_to_eth()" but with a BNB url
 	# w3 = 0
-	url = "https://bsc-testnet.core.chainstack.com/f00a33dd51b1a914ef0d397f6de98d80"
+	url = "https://bsc-testnet.nownodes.io"
 	w3 = Web3(HTTPProvider(url))
 	assert w3.is_connected(), f"Failed to connect to provider at {url}"
 
